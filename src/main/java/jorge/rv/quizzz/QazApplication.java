fd839e37d6b7e48f91a301300604c820c528efaa
richparen <richparen@gmail.com>
2021-03-15 15:34:31 +0600

    first commit

---
+++ src/main/java/jorge/rv/quizzz/QazApplication.java
@@ -0,0 +1,13 @@
+package jorge.rv.quizzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QazApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QazApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jorge/rv/quizzz/QazApplicationTests.java
@@ -0,0 +1,13 @@
+package jorge.rv.quizzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QazApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
